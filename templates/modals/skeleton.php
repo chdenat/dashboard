9376747c379d590b23dd7c7a14db4ee629dedec0
chdenat <christian.denat@orange.fr>
2022-09-15 11:30:30 +0200

    - suppress escape action

--- templates/modals/skeleton.php
+++ templates/modals/skeleton.php
@@ -21,7 +21,7 @@
 
 <?= Template::instance()->comments( "user management modal" ) ?>
 
-<div class="modal fade"  data-bs-backdrop="static"  id="dashboard-modal" tabindex="-1"
+<div class="modal fade"  data-bs-backdrop="static" data-bs-keyboard="false" id="dashboard-modal" tabindex="-1"
      aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
         <div class="modal-content">
