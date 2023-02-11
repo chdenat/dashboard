f84bb765314c539767fd974abf6b8a9fce05f24a
chdenat <christian.denat@orange.fr>
2023-02-11 18:24:15 +0100

    - add constant and unloading method , clean remove

--- assets/js/Animation.js
+++ assets/js/Animation.js
@@ -16,42 +16,62 @@ import {Template} from "./Template.js";
 
 
 class Animation {
-    
-    static #template
 
+    static #template
+    static classes = {
+        unloading: 'unloading',
+        loading: 'loading',
+        loaded: 'loaded',
+        running: 'running',
+    }
 
     static get #loader() {
         return document.getElementById(this.#template.animation_type())
-}
+    }
+    static #remove_classes = (container) => {
+        container.classList.remove(...Object.values(Animation.classes))
+    }
 
     static loading(arg) {
 
-        let template_id = (arg instanceof Event) ?arg.template.ID :arg
+        let template_id = (arg instanceof Event) ? arg.template.ID : arg
         this.#template = new Template(template_id)
         let loader = Animation.#loader
 
         if (this.#template.animate() && loader) {
-            if ( document.querySelector(`[data-template-id="${template_id}"]`).classList.contains('loaded')) {
-                document.querySelector(`[data-template-id="${template_id}"]`).classList.replace('loaded','loading')
-            } else {
-                document.querySelector(`[data-template-id="${template_id}"]`).classList.add('loading')
-            }
-           loader?.classList.add('running')
+            let container = document.querySelector(`[data-template-id="${template_id}"]`)
+            Animation.#remove_classes(container)
+            container.classList.add(Animation.classes.loading)
+            loader?.classList.add(Animation.classes.running)
         }
     }
 
     static loaded(arg) {
 
-        let template_id = (arg instanceof Event) ?arg.template.ID:arg
+        let template_id = (arg instanceof Event) ? arg.template.ID : arg
         this.#template = new Template(template_id)
         let loader = Animation.#loader
 
         if (this.#template.animate() && loader) {
-            document.querySelector(`[data-template-id="${template_id}"]`).classList.replace('loading','loaded')
-            loader.classList.remove('running')
+            let container = document.querySelector(`[data-template-id="${template_id}"]`)
+            Animation.#remove_classes(container)
+            container.classList.add(Animation.classes.loaded)
+            loader.classList.remove(Animation.classes.running)
         }
     }
 
+    static unloading(arg) {
+        let template_id = (arg instanceof Event) ? arg.template.ID : arg
+        this.#template = new Template(template_id)
+        let loader = Animation.#loader
+
+        if (this.#template.animate() && loader) {
+            let container = document.querySelector(`[data-template-id="${template_id}"]`)
+            Animation.#remove_classes(container)
+            container.classList.add(Animation.classes.unloading)
+            loader.classList.remove(Animation.classes.running)
+        }
+    }
 }
 
 export {Animation}
