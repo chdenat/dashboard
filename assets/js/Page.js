7c3c5f37d3de7496cad4cf6317d95054183668ab
chdenat <christian.denat@orange.fr>
2023-02-19 17:29:22 +0100

    - add comment for optional methods

--- assets/js/Page.js
+++ assets/js/Page.js
@@ -18,7 +18,7 @@ import {dsb} from "dsb";
 export class Page {
 
     #template = null
-    #page= null
+    #page = null
 
     constructor(page, template) {
         this.#page = page
@@ -27,31 +27,6 @@ export class Page {
 
         //import sheet from './style.css' assert { type: 'css' };
         //document.adoptedStyleSheets = [sheet];
-    }
-
-    /**
-     * Those 2 methods could be declared in the Cpntroller Page Class
-     *
-     * They are automatically called when we import the class and instantiate the controller
-     *
-
-     static globalPageInitialisation  = ()=> {
-        console.error(`You must implement "globalPageInitialisation" method !`)
-    }
-     attachEvents  = ()=> {
-        throw new Error(`You must implement "attachEvents" method !`)
-    }
-
-     *
-     *
-     */
-
-    initialisation = () => {
-        //Silent mode :  this is called when there is no child method
-    }
-
-    loaded() {
-        Animation.loaded('#content#')
     }
 
     /**
@@ -70,6 +45,39 @@ export class Page {
      */
     get page() {
         return this.#page
+    }
+
+    /**
+     * Those methods could be declared in the Controller Page Class
+     * They are automatically called when we import the class and instantiate the controller
+     *
+     * Ordering :
+     *      - import JS Page Controller          // in Dashboard class
+     *      - call globalPageInitialisation      // call in Dashboard class
+     *      - call attachEvents                  // call in Dashboard class
+     *      - load Page                          // in Dashboard class
+     *
+     *      - call pageInitialisation            // call in Template
+
+     * This is a  static method used to make some global initialisation
+
+     static globalPageInitialisation  = ()=> {}
+
+     * This method initialise page data
+
+     pageInitialisation = () => {}
+
+     * This is a method used to attach events before we load the page
+
+     attachEvents  = ()=> {}
+
+     *
+     *
+     */
+
+
+    loaded() {
+        Animation.loaded('#content#')
     }
 
 }
