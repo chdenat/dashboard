931f1f6fe761cccab2e0b620e4850eb09aa8d370
chdenat <christian.denat@orange.fr>
2023-02-21 10:25:03 +0100

    - add export chart error messages

--- settings/texts-4-i18n.php
+++ settings/texts-4-i18n.php
@@ -1,5 +1,5 @@
 <?php
-	/**********************************************************************************************************************
+/**********************************************************************************************************************
  *                                                                                                                    *
  * Project : dashboard                                                                                                *
  * File : texts-4-i18n.php                                                                                            *
@@ -7,35 +7,35 @@
  * @author: Christian Denat                                                                                           *
  * @email: contact@noleam.fr                                                                                          *
  *                                                                                                                    *
- * Last updated on : 20/02/2023  20:12                                                                                *
+ * Last updated on : 21/02/2023  09:35                                                                                *
  *                                                                                                                    *
  * Copyright (c) 2023 - noleam.fr                                                                                     *
  *                                                                                                                    *
  **********************************************************************************************************************/
-	
-	/**
-	 * Define texts to translate in a <text-i18n></text-i18n> pseudo HTML element and then to be used
-	 * in Javascript modules.
-	 *
-	 * IN HTML, in this file, you should declare the texts to translate.
-	 *
-	 *      <text-i18n context="<some-context>"
-	 *                 data-title="<?= _( '<some title here>' ) ?>"
-	 *                 data-text="<?= _( '<some text here>' ) ?>"
-	 *                 data-other="<?= _( '<some other text here>' ) ?>">
-	 *      </text-i18n>
-	 *
-	 *
-	 * Then, on JS side us  dsb.ui.get_text_i18n  function to retrieve translated texts.
-	 *
-	 *       dsb.ui.get_text_i18n(<some-context>')         :  {
-	 *                                                              title:translation of '<some title here>'
-	 *                                                              text : translation of '<some text here>'
-	 *                                                        }
-	 *       dsb.ui.get_text_i18n(<some-context>','title') :  translation of '<some title here>'
-	 *       dsb.ui.get_text_i18n(<some-context>').title   :  translation of '<some title here>'
-	 *
-	 */
+
+/**
+ * Define texts to translate in a <text-i18n></text-i18n> pseudo HTML element and then to be used
+ * in Javascript modules.
+ *
+ * IN HTML, in this file, you should declare the texts to translate.
+ *
+ *      <text-i18n context="<some-context>"
+ *                 data-title="<?= _( '<some title here>' ) ?>"
+ *                 data-text="<?= _( '<some text here>' ) ?>"
+ *                 data-other="<?= _( '<some other text here>' ) ?>">
+ *      </text-i18n>
+ *
+ *
+ * Then, on JS side us  dsb.ui.get_text_i18n  function to retrieve translated texts.
+ *
+ *       dsb.ui.get_text_i18n(<some-context>')         :  {
+ *                                                              title:translation of '<some title here>'
+ *                                                              text : translation of '<some text here>'
+ *                                                        }
+ *       dsb.ui.get_text_i18n(<some-context>','title') :  translation of '<some title here>'
+ *       dsb.ui.get_text_i18n(<some-context>').title   :  translation of '<some title here>'
+ *
+ */
 ?>
 
 <!--
@@ -43,18 +43,18 @@ User login, logout and change password
 -->
 
 <text-i18n context="user/log-in"
-           data-title="<?= _( 'Log in' ) ?>"
-          data-text="<?= _( 'User %s has been logged in successfully!' ) ?>">
+           data-title="<?= _('Log in') ?>"
+           data-text="<?= _('User %s has been logged in successfully!') ?>">
 </text-i18n>
 
 <text-i18n context="user/log-out"
-          data-title="<?= _( 'Log Out' ) ?>"
-          data-text="<?= _( 'User %s has been logged out!' ) ?>">
+           data-title="<?= _('Log Out') ?>"
+           data-text="<?= _('User %s has been logged out!') ?>">
 </text-i18n>
 
 <text-i18n context="user/new-password"
-           data-title="<?= _( 'New password' ) ?>"
-           data-text="<?= _( 'Password changed for user %s.' ) ?>">
+           data-title="<?= _('New password') ?>"
+           data-text="<?= _('Password changed for user %s.') ?>">
 </text-i18n>
 
 <!--
@@ -84,12 +84,16 @@ APEX CHART Translation
 
 <text-i18n context="chart/csv"
            data-title="<?= _('Export data') ?>"
-           data-text="<?= _('The chart data has been exported to CSV!') ?>">
+           data-text="<?= _('The chart data has been exported to CSV!') ?>"
+           data-error="<?= _('Error during the CSV export!') ?>">
 </text-i18n>
+
 <text-i18n context="chart/svg"
            data-title="<?= _('Export Chart') ?>"
-           data-text="<?= _('The chart has been exported to SVG format!') ?>">
+           data-text="<?= _('The chart has been exported to SVG format!') ?>"
+           data-error="<?= _('Error during the SVG export!') ?>">
 </text-i18n>
+
 <text-i18n context="toolbar/csv" data-text="<?= _('Export data in CSV format') ?>"></text-i18n>
 <text-i18n context="toolbar/svg" data-text="<?= _('Export chart in SVG format') ?>"></text-i18n>
 <text-i18n context="toolbar/zoom" data-text="<?= _('Selection Zoom') ?>"></text-i18n>
