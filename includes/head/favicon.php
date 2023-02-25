d7acc5455cd62d2c9dc2bc50526aa183d5c745a1
chdenat <christian.denat@orange.fr>
2023-02-25 11:59:46 +0100

    - remove manifest

--- includes/head/favicon.php
+++ includes/head/favicon.php
@@ -1,30 +1,29 @@
 <?php
 	
-	/***********************************************************************************************************************
-	 *
-	 * Project : supervix4
-	 * file : cache.php
-	 *
-	 * @author        Christian Denat
-	 * @email contact@noleam.fr
-	 * --
-	 *
-	 * updated on :  1/18/22, 11:20 AM
-	 *
-	 * @copyright (c) 2022 noleam.fr
-	 *
-	 **********************************************************************************************************************/
-	
-	use dashboard\template\Template;
-	
-	ob_start();
-	echo Template::instance()->comments( 'fav icon' );
+	/**********************************************************************************************************************
+ *                                                                                                                    *
+ * Project : dashboard                                                                                                *
+ * File : favicon.php                                                                                                 *
+ *                                                                                                                    *
+ * @author: Christian Denat                                                                                           *
+ * @email: contact@noleam.fr                                                                                          *
+ *                                                                                                                    *
+ * Last updated on : 25/02/2023  11:59                                                                                *
+ *                                                                                                                    *
+ * Copyright (c) 2023 - noleam.fr                                                                                     *
+ *                                                                                                                    *
+ **********************************************************************************************************************/
+
+use dashboard\template\Template;
+
+ob_start();
+echo Template::instance()->comments('fav icon');
 ?>
     <link rel="icon" href="data:,">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?=FAVICON?>apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?=FAVICON?>favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?=FAVICON?>favicon-16x16.png">
-    <link rel="manifest" href="<?=FAVICON?>site.webmanifest">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= FAVICON ?>apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= FAVICON ?>favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= FAVICON ?>favicon-16x16.png">
+    <!--    <link rel="manifest" href="--><?php //=FAVICON?><!--site.webmanifest">-->
 <?php
-	echo Template::instance()->end_comments();
-	echo ob_get_clean();
+echo Template::instance()->end_comments();
+echo ob_get_clean();
